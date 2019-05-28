3f4e5bb298622c51d3b16cda4aa7cb4e8d67ea7f
wb-ypp037115 <wb-ypp037115@antfin.com>
2019-05-28 15:39:00 +0800

    fix

--- server/app.js
+++ server/app.js
@@ -2,19 +2,10 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var path =require("path");
-var chokidar = require('chokidar');
 require('console-stamp')(console, "HH:MM:ss.l");
  
 if(process.env.NODE_ENV==="development"){
     require("./webpack-hot-replace")(app);
-    if(process.env.WATCH){
-        chokidar.watch('webpack.dev.config.js').on('change', function(){
-            console.warn('webpack.dev.config.js > change.....');
-            process.exit(1);
-            console.info('exit process');
-            process.exec("tnpm run start:node:dev");
-        });
-    }
 }else{
     app.use(express.static(path.join(process.cwd(),"public")));
 }
